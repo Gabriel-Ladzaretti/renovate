a6a1b4b1728194234e714fe146276e36a876f431
Gabriel-Ladzaretti <Gabriel.ladzaretti@whitesourcesoftware.com>
2022-07-01 23:24:12 +0300

    promisify spawn

--- lib/util/exec/spawn-driver.ts
+++ lib/util/exec/spawn-driver.ts
@@ -3,6 +3,7 @@ import { rawSpawn } from './common';
 import type { RawSpawnOptions } from './types';
 
 void (async () => {
+  const cmds: string[] = [];
   const opts: RawSpawnOptions = {
     encoding: 'utf8',
     shell: true,
@@ -10,21 +11,29 @@ void (async () => {
   };
   logger.info('driver function - START');
   logger.info({ options: opts }, 'spawn options');
-  try {
-    const cmd = 'npm run non-existent-script';
-    // const cmd = 'docker';
-    // const cmd = 'ls /usr';
-    // const cmd = 'npm run spawn-testing-script';
-    logger.info(`driver function - RUN - "${cmd}"`);
-    const { stdout, stderr } = await rawSpawn(cmd, opts);
-    if (stdout) {
-      logger.info(stdout);
+  cmds.push('npm run non-existent-script');
+  cmds.push('docker');
+  cmds.push('npm run spawn-testing-script');
+  cmds.push('ls /usr');
+
+  for (const cmd of cmds) {
+    logger.info(`run cmd - START - "${cmd}"`);
+    try {
+      // const cmd = 'npm run non-existent-script';
+      // const cmd = 'docker';
+      // const cmd = 'ls /usr';
+      // const cmd = 'npm run spawn-testing-script';
+      const { stdout, stderr } = await rawSpawn(cmd, opts);
+      if (stdout) {
+        logger.info(stdout);
+      }
+      if (stderr) {
+        logger.warn(stderr);
+      }
+    } catch (err) {
+      logger.error(err as string);
     }
-    if (stderr) {
-      logger.warn(stderr);
-    }
-  } catch (err) {
-    logger.error(err as string);
+    logger.info(`run cmd - END - "${cmd}"`);
   }
   logger.info('driver function - END');
 })();
